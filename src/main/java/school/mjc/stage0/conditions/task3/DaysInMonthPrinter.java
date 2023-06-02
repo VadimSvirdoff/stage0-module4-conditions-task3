426857e54a3c182f4a097918ac4dcaa53831770e
Violetta Prikhno <Violetta_Prikhno@epam.com>
2023-06-02 12:41:29 +0300

    My first commit!

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,27 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        if (month >= 1 && month <= 12) {
+            int days;
+
+            switch (month) {
+                case 2:
+                    days = 28;
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    days = 30;
+                    break;
+                default:
+                    days = 31;
+                    break;
+            }
+
+            System.out.println("Number of days in month " + month + ": " + days);
+        } else {
+            System.out.println("Wrong number!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary < 0) {
+            System.out.println("wrong input!");
+        } else if (salary <= 10000) {
+            double tax = salary * 0.15;
+            double netSalary = salary - tax;
+            System.out.println(netSalary);
+        } else if (salary <= 20000) {
+            double tax = salary * 0.18;
+            double netSalary = salary - tax;
+            System.out.println(netSalary);
+        } else {
+            double tax = salary * 0.20;
+            double netSalary = salary - tax;
+            System.out.println(netSalary);
+        }
     }
 }
